acca5b76eb49c666d6096a18ab21b5a1cb0ce602
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2022-04-22 03:34:00 +0530

    add schema and model

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = new Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
