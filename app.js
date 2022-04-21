98ec9bf558208c9a29c92400112c4778606de3ad
Chaitanya Makala <chaitanya.m.1996@gmail.com>
2022-04-22 03:37:33 +0530

    add public and views folder

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
-const app = express();
-
 const mongoose = require("mongoose");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 const { Schema } = mongoose;
 mongoose.connect("mongodb://localhost:27017/wikiDB");
 
